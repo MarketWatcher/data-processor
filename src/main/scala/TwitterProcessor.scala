db8bd0cc6484ed20d2e273c2a59649359db283db
Ayse Yorusun <ayorusun@euayorusan.local>
2016-08-12 17:20:49 +0300

    @oalmali add spark jobserver integration

--- src/main/scala/TwitterProcessor.scala
+++ src/main/scala/TwitterProcessor.scala
@@ -3,7 +3,7 @@ import java.util.{Date, UUID}
 import _root_.kafka.serializer.StringDecoder
 import com.datastax.spark.connector.SomeColumns
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.streaming.dstream.{InputDStream, DStream}
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.twitter.TwitterUtils
 
@@ -14,48 +14,66 @@ import org.apache.spark.streaming._
 import com.datastax.spark.connector.streaming._
 import org.joda.time.{DateTime, DateTimeZone}
 import com.datastax.driver.core.utils.UUIDs
+import com.typesafe.config.{Config, ConfigFactory}
+import spark.jobserver.{SparkJob, SparkJobValid, SparkJobValidation, ContextLike}
 
 
-object TwitterProcessor {
+object TwitterProcessor extends SparkJob {
 
   var batch_interval_in_seconds = 1
   var window_in_minutes = 60
-  var slide_in_seconds = 30
+  var slide_in_seconds = 10
   var alertID: UUID = _
   var requiredKeys: Array[String] = _
   var niceToHaveKeys: Array[String] = _
 
-  val cassandraHost: String = "marketwatcher.tech"
-
   def main(args: Array[String]) {
-
+/*
     println("args.length: " + args.length)
-    if (args.length < 4) {
-      System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads>")
+    if (args.length < 5) {
+      System.err.println("Usage: KafkaWordCount <zkQuorum> <group> <topics> <numThreads> <cassandraHost>")
       System.exit(1)
     }
 
-    val Array(zkQuorum, group, topics, numThreads) = args
+    val Array(zkQuorum, group, topics, numThreads, cassandraHost) = args*/
 
-    val conf = new SparkConf().setMaster("local[*]").setAppName("data-processor: ")
-    conf.set("spark.cassandra.connection.host", cassandraHost)
-    val ssc = new StreamingContext(conf, Seconds(batch_interval_in_seconds))
+   // val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+
+    val conf = new SparkConf().setMaster("local[4]").setAppName("data-processor: ")
+    conf.set("spark.cassandra.connection.host", "db")
+
+    val sc = new SparkContext(conf)
+
+    val config = ConfigFactory.parseString("")
+    val results = runJob(sc, config)
+
+
+  }
+
+
+  override def runJob(sc: SparkContext, jobConfig: Config): Any = {
+
+    val topics = "test"
+
+    val topicMap = topics.split(",").map((_, 2)).toMap
+
+    val ssc = new StreamingContext(sc.getConf, Seconds(batch_interval_in_seconds))
     ssc.checkpoint("checkpoint")
 
-    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
-    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
-
-    lines.print()
+    alertID = UUIDs.timeBased()
+    val lines = KafkaUtils.createStream(ssc, "localhost:2181", "ayse-group", topicMap).map(_._2)
+    val counts = getCountStreamOfTweets(lines)
+    counts.saveToCassandra("trends", "alert_trend", SomeColumns("id", "alert_id", "count", "process_date"))
 
     ssc.start()
 
     ssc.awaitTermination()
 
-    alertID = UUIDs.timeBased()
-
-    //getCountStreamOfTweets(lines).saveToCassandra("marketwatcher", "alert_trend", SomeColumns("id", "alert_id", "count", "process_date"))
-
   }
+
+
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = SparkJobValid
+
 
   def countByWindow(matchedTweets: DStream[String], windowDuration: Long, slideDuration: Long): DStream[Long] = {
     matchedTweets.countByWindow(Seconds(windowDuration), Seconds(slideDuration))
